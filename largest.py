d1d136ee6c2fc83c19a70d4808291292e1c053dd
Naved7467 <ns282949@gmail.com>
2025-06-30 15:48:27 +0530

    Update largest.py complete my code

--- largest.py
+++ largest.py
@@ -1,1 +1,9 @@
-num1 = int(input("enter the num"))
+num1 = int(input("enter the num:"))
+num2 = int(input("enter the num:"))
+num3 = int(input("enter the num:"))
+if num1 > num2 and num1 > num3:
+    print ("num1")
+elif  num2 > num1 and num2 > num3:
+     print ("num2")
+else:
+     print ("num3")
