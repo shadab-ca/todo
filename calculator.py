46061c38d3dc72aa31160c9c95a7d6c99364d978
Shoaib9458 <shoaibqureshi48930@gmail.com>
2026-02-09 11:09:52 +0530

    Update calculator.py updated

--- calculator.py
+++ calculator.py
@@ -1,71 +1,66 @@
-def sum(a,b): 
-    s = a + b
-    return s
-    
-def sub(a,b):
-    s = a-b
-    return s
-    
-def mult(a,b):
-    m = a*b
-    return m
+def add(a, b):
+    return a + b
 
-def div(a,b):
-    d = a/b
-    return d
+def sub(a, b):
+    return a - b
 
+def mult(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
 
 def check_even_odd(num):
     if num % 2 == 0:
         print(num, "is even")
     else:
-        print(num, "is odd") 
+        print(num, "is odd")
 
 def factorial(a):
-    factorial = 1
-    if  a < 0:
-        print("sorry")
-    elif a == 0:
-        print("factorial")
-    else:  
-        for i in range(1,a + 1):
-            factorial = factorial*i
-        print("factorial",factorial)
+    if a < 0:
+        return "sorry"
+    fact = 1
+    for i in range(1, a + 1):
+        fact *= i
+    return fact
 
-def prime(num)
-     naved = False
+def prime(num):
     if num == 0 or num == 1:
-       print(num,"is not prime num")
-    elif: num > 1:
-      
-      for i in range(2,num)
-            
-oper = int(input("opertion:"))
-num1 = int(input("enter the num1:"))
-num2 = int(input("ente the num2:"))
-value = 0
+        print(num, "is not a prime number")
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                print(num, "is not a prime number")
+                return
+        print(num, "is a prime number")
+
+
+oper = int(input("operation: "))
+num1 = int(input("enter num1: "))
+num2 = int(input("enter num2: "))
+
+value = None
+
 if oper == 1:
-    value =  sum(num1, num2)
+    value = add(num1, num2)
 elif oper == 2:
-    value = sub(num1,num2)
-
-elif oper == 3: 
-    value = mult(num1,num2)
-
+    value = sub(num1, num2)
+elif oper == 3:
+    value = mult(num1, num2)
 elif oper == 4:
-    value = div(num1,num2)
-
+    value = div(num1, num2)
 elif oper == 5:
     value = factorial(num1)
-    
 elif oper == 6:
     check_even_odd(num1)
-    check_even_odd(num2)    
+    check_even_odd(num2)
+elif oper == 7:
+    prime(num1)
 
-elif oper = 7:
-   value = prime  
-    
-print("value=",value)
-    
+if value is not None:
+    print("value =", value)
+
 
  
